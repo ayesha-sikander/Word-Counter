f760c2dd24d11960ab26ef479965b094a993bc29
ayesha-sikander <aashii0305@gmail.com>
2024-04-17 22:03:59 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.yellow.italic("Welcome to Code-With-Ayesha-Sikander - WordCounter"));
+let answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: chalk.green("Enter your Sentence to Count the Word: ")
+    }
+])
+
+let words = answers.Sentence.trim().split(" ")
+console.log(chalk.redBright(`Your Sentence Word Count is ${words.length}`));
